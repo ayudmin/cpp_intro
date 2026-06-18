ed21a2ce150b9cb63a3270d877be2f26ac0bbf06
ayudmin <vikrammehra244@gmail.com>
2026-06-18 17:33:29 +0300

    info: hello world print [* learning *]

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
+// writing a program that prints
+// hello world to the standard output
 #include <iostream>
 
-int main (){
-    std::cout << "enter first number: ";
-    int v1;
-    std::cin >> v1;
-    int v2;
-    std::cout << "enter second number: ";
-    std::cin >> v2,
-    std::cout << "The sum of " << v1 << " and " << v2
-              << " is " << v1 + v2 << std::endl;
+int main(){
+    std::cout << "hello world" << std::endl;
     return 0;
 }
