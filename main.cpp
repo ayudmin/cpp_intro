86355e87889f87520fc0bbd0c9ca4dc43cc15382
ayudmin <vikrammehra244@gmail.com>
2026-06-18 17:53:45 +0300

    info: a program that multiplies user defined numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-// writing a program that prints
-// hello world to the standard output
+// writing a program that multiplies two numbers
+// with user passed data
 #include <iostream>
 
 int main(){
-    std::cout << "hello world" << std::endl;
+    std::cout << "enter first number: ";
+    int number_1;
+    std::cin >> number_1;
+    std::cout << "enter second number: ";
+    int number_2;
+    std::cin >> number_2;
+    std::cout << "the product of " << number_1 << "*" << number_2 << " is: " << number_1 * number_2 << std::endl;
     return 0;
 }
