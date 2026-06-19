4c14a6c7f7310ed5cd54aafe5853a8b8662211c3
ayudmin <vikrammehra244@gmail.com>
2026-06-19 07:02:49 +0300

    info: invalid comment

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-// writing a program that multiplies two numbers
-// with user passed data
 #include <iostream>
 
+/*
+*   double line comment explaining about project
+*   here am testing comment nesting */      */
+*   and basically for medium to long code commenting
+*   or explaination
+*/
+
 int main(){
-    std::cout << "enter first number: ";
-    int number_1;
-    std::cin >> number_1;
-    std::cout << "enter second number: ";
-    int number_2;
-    std::cin >> number_2;
-    std::cout << "the product of " << number_1 << "*" << number_2 << " is: " << number_1 * number_2 << std::endl;
     return 0;
 }
+
+
