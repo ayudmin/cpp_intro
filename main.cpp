096a2858712d7cea27c139c1e63bc7601f6ad6f1
ayudmin <vikrammehra244@gmail.com>
2026-06-19 16:40:03 +0300

    info: while loop [ sum of 1 to 10 inclusive ]

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
 /*
-*   double line comment explaining about project
-*   here am testing comment nesting */      */
-*   and basically for medium to long code commenting
-*   or explaination
+* while statement
 */
 
 int main(){
+    int sum = 0, val = 1;
+    // keep executing the while as long as val is less than or equal to sum
+    while (val <= 10){
+        std::cout << "starting loop" << std::endl;
+        std::cout << "sum is: " << sum << std::endl;
+        std::cout << "val is: " << val << std::endl;
+        sum += val;
+        std::cout << "sum is: " << sum << std::endl;
+        std::cout << "val is: " << val << std::endl;
+        ++val;
+        std::cout << "sum is: " << sum << std::endl;
+        std::cout << "val is: " << val << std::endl;
+    }
+    std::cout << "sum of 1 to 10 inclusive is: " << sum << std::endl;
     return 0;
 }
-
-
