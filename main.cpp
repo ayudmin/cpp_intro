4f83902a8a8193f74ab2728b41293ac87c97be53
ayudmin <vikrammehra244@gmail.com>
2026-06-21 18:18:26 +0300

    info: decreasing with while loops

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+
+/*
+* a program that uses the decreament operator
+* in a loop state [ while ]
+*/
+
 int main(){
-    int sum = 0, val = 49;
-    while (val <= 99){
-        sum += val;
-        ++val;
-        std::cout << val << std::endl;
+    int sum = 0, value = 10;
+    while(value >= 1){
+    std::cout << value << std::endl;
+        sum += value;
+        --value;
     }
     return 0;
 }
