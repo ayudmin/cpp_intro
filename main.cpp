5262278388bb66fb9c54bffd784fc6ca9a6229af
ayudmin <vikrammehra244@gmail.com>
2026-06-21 18:17:59 +0300

    info: while loop

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-
-/*
-* while statement
-*/
-
 int main(){
-    int sum = 0, val = 1;
-    // keep executing the while as long as val is less than or equal to sum
-    while (val <= 10){
-        std::cout << "starting loop" << std::endl;
-        std::cout << "sum is: " << sum << std::endl;
-        std::cout << "val is: " << val << std::endl;
+    int sum = 0, val = 49;
+    while (val <= 99){
         sum += val;
-        std::cout << "sum is: " << sum << std::endl;
-        std::cout << "val is: " << val << std::endl;
         ++val;
-        std::cout << "sum is: " << sum << std::endl;
-        std::cout << "val is: " << val << std::endl;
+        std::cout << val << std::endl;
     }
-    std::cout << "sum of 1 to 10 inclusive is: " << sum << std::endl;
     return 0;
 }
