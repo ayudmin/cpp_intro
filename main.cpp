bc35879d03ca3b4194985717a8839682d3539643
ayudmin <vikrammehra244@gmail.com>
2026-06-22 06:56:08 +0300

    info: summing numbers from through 10 with for loops

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
-/*
-* a program that uses the decreament operator
-* in a loop state [ while ]
-*/
+// iterating using for loops
 
 int main(){
-    int sum = 0, value = 10;
-    while(value >= 1){
-    std::cout << value << std::endl;
-        sum += value;
-        --value;
-    }
+    int sum = 0; // sum value from 1 through 10
+    for (int val = 1; val <= 10; ++val)
+        sum += val;
+        std::cout << "sum of 1 through 10 is: " << sum << std::endl;
     return 0;
 }
+
+
